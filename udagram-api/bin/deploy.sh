642348a3243949558945a9b551446625b0528501
Fuh Austin <fuhaustin@gmail.com>
2021-04-11 19:16:19 +0100

    feat: update aws passing key id and secret in env file

--- udagram-api/bin/deploy.sh
+++ udagram-api/bin/deploy.sh
@@ -1,6 +1,2 @@
 # eb init udagram-server-env --region=us-east-1   && eb deploy && eb status
-# eb use udagram-server-env && eb deploy && eb status
-
-#  eb init udagram-server-env --platform node.js
-
-eb init udagram-server-env --platform node.js  && eb use udagram-server-env &&  eb deploy && eb status
+eb create udagram-server-env && eb deploy && eb status
