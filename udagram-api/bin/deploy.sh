b44f176790f7701a61355ac9358a5ab96dad10ff
Fuh Austin <fuhaustin@gmail.com>
2021-04-12 11:57:19 +0100

    feat: Test deployment

--- udagram-api/bin/deploy.sh
+++ udagram-api/bin/deploy.sh
@@ -8,6 +8,8 @@
 
 # eb init udagram-server-env --region=us-east-1   && eb deploy && eb status
 # eb init udagram-server-env --region=us-east-1 && eb use udagram-server-env && eb deploy && eb status
-#eb init udagram-server  --region=us-east-1 --platform node.js && eb use udagram-server-env && eb deploy && eb status
+eb init udagram-server  --region=us-east-1 --platform node.js-12 && eb use udagram-server-env && eb deploy && eb status
 
-eb use udagram-server-env && eb deploy && eb status
+# eb use udagram-server-env && eb deploy && eb status
+
+# eb platform list --region=us-east-1
