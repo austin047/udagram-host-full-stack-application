0553626e86cc87917abf4437cdb3f3587081bb97
Fuh Austin <fuhaustin@gmail.com>
2021-04-12 09:02:46 +0100

    feat: Update circle ci

--- udagram-api/bin/deploy.sh
+++ udagram-api/bin/deploy.sh
@@ -8,5 +8,6 @@
 
 # eb init udagram-server-env --region=us-east-1   && eb deploy && eb status
 # eb init udagram-server-env --region=us-east-1 && eb use udagram-server-env && eb deploy && eb status
-eb init udagram-server  --region=us-east-1 --platform node.js && eb use udagram-server-env && eb deploy && eb status
+#eb init udagram-server  --region=us-east-1 --platform node.js && eb use udagram-server-env && eb deploy && eb status
 
+eb use udagram-server && eb deploy && eb status
